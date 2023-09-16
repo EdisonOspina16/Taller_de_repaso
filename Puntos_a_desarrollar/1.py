9e18aa6ef010bcb957409b611361add188ae7d33
edisonospina16 <edison100ospina@gmail.com>
2023-09-15 20:45:01 -0500

    final

--- Puntos_a_desarrollar/1.py
+++ Puntos_a_desarrollar/1.py
@@ -10,3 +10,47 @@ class Elemento:
         return False
 
 
+class Conjunto:
+    contador = 0
+
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.elementos = []
+        Conjunto.contador += 1
+        self.__id = Conjunto.contador
+
+    @property
+    def id(self):
+        return self.__id
+
+    def contiene(self, elemento):
+        return any(e == elemento for e in self.elementos)
+
+    def agregar_elemento(self, elemento):
+        if not self.contiene(elemento):
+            self.elementos.append(elemento)
+
+    def unir(self, otro_conjunto):
+        for elemento in otro_conjunto.elementos:
+            self.agregar_elemento(elemento)
+
+    def __add__(self, otro_conjunto):
+        nuevo_conjunto = Conjunto(f"{self.nombre} UNIDO {otro_conjunto.nombre}")
+        nuevo_conjunto.elementos = self.elementos.copy()
+        nuevo_conjunto.unir(otro_conjunto)
+        return nuevo_conjunto
+
+    @classmethod
+    def intersectar(cls, conjunto1, conjunto2):
+        elementos_comunes = [elemento for elemento in conjunto1.elementos if conjunto2.contiene(elemento)]
+        nombre_resultante = f"{conjunto1.nombre} INTERSECTADO {conjunto2.nombre}"
+        nuevo_conjunto = Conjunto(nombre_resultante)
+        nuevo_conjunto.elementos = elementos_comunes
+        return nuevo_conjunto
+
+    def __str__(self):
+        elementos_str = ", ".join([elemento.nombre for elemento in self.elementos])
+        return f"Conjunto {self.nombre}: ({elementos_str})"
+
+
+
